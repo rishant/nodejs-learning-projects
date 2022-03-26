78eead565b2f80a2ad546db7ea8b37ca98350ddc
Rishant <rishantgupta007@gmail.com>
2022-03-27 00:23:42 +0530

    Adding logger for print env information of cross-env multi environment profiling

--- nodejs-mysql-orm-sequelize-example/util/database.js
+++ nodejs-mysql-orm-sequelize-example/util/database.js
@@ -4,6 +4,8 @@ const Sequelize = require("sequelize");
 const envPath = process.env.NODE_ENV == null ? `.env`: `.env.${process.env.NODE_ENV}`;
 require('dotenv').config({ path: envPath })
 
+console.log(process.env.APP_ENV);
+
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
